4847077f658eba8b4f82b4508fc538edf5a92f15
Hobin Yoon <hobinyoon@gmail.com>
2013-03-31 08:06:13 -0400

    year and month to the output.

--- src/tcptable.c
+++ src/tcptable.c
@@ -628,8 +628,9 @@ void tcp_map_flush(int logging, FILE* logfile, const time_t last_tm)
 	Btps* i;
 	for (i = BTPS_HEAD; i != NULL; )
 	{
-		fprintf(logfile, "NS: %02d-%02d%02d%02d; %s %u %lu\n",
-				tm_->tm_mday, tm_->tm_hour, tm_->tm_min, tm_->tm_sec,
+		fprintf(logfile, "NS: %02d%02d%02d-%02d%02d%02d; %s %u %lu\n",
+				tm_->tm_year, tm_->tm_mon, tm_->tm_mday,
+				tm_->tm_hour, tm_->tm_min, tm_->tm_sec,
 				i->fromto, i->bytes, i->bytes_total);
 		Btps* j = i;
 		i = i->next;
